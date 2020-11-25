4f9c3f4151e882c7a0db8fede13e60a15ff7c300
Osni Silva Junior <69945883+osnijr96@users.noreply.github.com>
2020-11-25 13:36:38 -0300

    Update form.php

--- app/views/home/form.php
+++ app/views/home/form.php
@@ -1,26 +1,48 @@
 <!-- CONTACT FORM -->
-<form id="contact-form" action="assets/php/contact.php" method="post">
-    <input id="contact-form-name" type="text" name="name" class="form-control scroll-animated-from-right"
-        placeholder="* Seu Nome" />
-
-    <input id="contact-form-email" type="text" name="email" class="form-control scroll-animated-from-right"
-        placeholder="* Seu Email" />
-
-    <!-- PHANTOM ELEMENT ( HONEYPOT CAPTCHA FOR SECURITY ) -->
-    <div class="fhp-input">
-        <input id="contact-form-company" type="text" name="company" class="form-control" />
+<form name="cadastro" action="http://localhost:8080/contact/adicionaAluno" method="POST">
+  <div class="form-group row">
+    <label class="col-4 col-form-label" for="nome">Nome</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-address-card"></i>
+          </div>
+        </div> 
+        <input id="nome" name="nome" placeholder="informe seu nome" type="text" class="form-control" aria-describedby="nomeHelpBlock" required="required">
+      </div> 
+      <span id="nomeHelpBlock" class="form-text text-muted">Informe seu nome completo</span>
     </div>
-    <!-- /PHANTOM ELEMENT ( HONEYPOT CAPTCHA FOR SECURITY ) -->
-
-    <textarea id="contact-form-message" name="message" class="form-control scroll-animated-from-right"
-        placeholder="* Mensagem"></textarea>
-
-    <button type="submit" class="form-control scroll-animated-from-right">
-        Enviar
-    </button>
-
-    <div class="success-message">* Mensagem Enviada!</div>
+  </div>
+  <div class="form-group row">
+    <label for="idade" class="col-4 col-form-label">Idade</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-bullseye"></i>
+          </div>
+        </div> 
+        <input id="idade" name="idade" placeholder="informe sua idade" type="text" required="required" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Sexo</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="radio" id="radio_0" type="radio" class="custom-control-input" value="masc"> 
+        <label for="radio_0" class="custom-control-label">Masculino</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="radio" id="radio_1" type="radio" class="custom-control-input" value="fem"> 
+        <label for="radio_1" class="custom-control-label">Feminino</label>
+      </div>
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+  </div>
 </form>
-<!-- /CONTACT FORM -->
-</section>
-<!-- /CONTACT -->
